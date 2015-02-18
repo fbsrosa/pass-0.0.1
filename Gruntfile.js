ba7a96b8b52bad11d205f320d0635ec87f044c82
fbsrosa <fbsrosa@gmail.com>
2015-02-18 01:36:44 -0200

    Setup Complete

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,58 @@
-module.exports = function (grunt) {
-    grunt.initConfig({
-        autoprefixer: {
-            dist: {
-                files: {
-                    'build/style.css': 'style.css'
-                }
-            }
-        },
-        watch: {
-            styles: {
-                files: ['style.css'],
-                tasks: ['autoprefixer']
-            }
-        }
-    });
-    grunt.loadNpmTasks('grunt-autoprefixer');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
-};
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.initConfig({
+            
+            uglify: {
+                my_target: {
+                    files: {
+                        '_/js/script.js': ['_/components/js/*.js'] 
+                    } // files
+                } // my_target
+            }, // uglify
+
+            compass: {
+                dev: {
+                    options: {
+                        config: 'config.rb'
+                    } //options
+                } // dev
+            }, // compass
+
+            autoprefixer: {
+                dev: {
+                    files: {
+                        '_/styles.css': ['_/components/sass/*.scss']
+                    } //files
+                } // dev
+            }, // autoprefixer
+            
+            watch : {
+                options: {livereload: true},
+                scripts: {
+                    files: ['_/components/js/*.js'],
+                    tasks: ['uglify']
+                }, // scripts
+
+                sass: {
+                    files: ['_/components/sass/*.scss'],
+                    tasks: ['compass:dev']
+                }, // sass
+
+                html: {
+                    files: ['*.html']
+                }, // html
+
+                styles: {
+                files: ['_/components/sass/*.scss'],
+                tasks: ['autoprefixer:dev']
+            }             
+            } // watch
+
+    }) //initConfig
+
+    grunt.registerTask('default', 'watch');      
+    
+}; //exports 
