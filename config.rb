6cde2acc723ce136e7a928bfb0cbb05147f04569
fbsrosa <fbsrosa@gmail.com>
2015-02-27 10:50:35 -0300

    Friday-Feb27-01

--- config.rb
+++ config.rb
@@ -1,6 +1,18 @@
 require "susy"
 
+# Set this to the root of your project when deployed:
+# http_path = "http://your-url-goes-here.com/path-to-your-directory/"
+
+# Set the images directory relative to your http_path or change
+# the location of the images themselves using http_images_path:
+# http_images_dir = "images"
+
+# Production Assets URL
+# http_images_path = "http://your-url-goes-here/img"
+
 css_dir = '_/css'
 sass_dir = '_/components/sass'
 javascripts_dir = '_/js'
+images_dir = "images"
 output_style = :compressed
+relative_assets = true
